9e822376017db6f4d9f34e60e400574da2aa3b1d
Loc <lochoang611@gmail.com>
2025-03-14 14:24:13 +0700

    first commit

---
+++ src/layouts/dashboard.tsx
@@ -0,0 +1,27 @@
+// Import components
+import { SidebarProvider, SidebarTrigger } from "src/components/ui/sidebar";
+import { AppSidebar } from "src/components/app-sidebar";
+import NotConnectWallet from "src/components/not-connect-wallet";
+import { useWallet } from "@razorlabs/razorkit";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const wallet = useWallet();
+  return (
+    <>
+      {!wallet.connected && <NotConnectWallet />}
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="py-2 pe-2 w-full h-screen max-h-screen">
+          <main className="flex flex-col w-full border rounded-lg overflow-hidden bg-background/70 backdrop-blur-md">
+            <SidebarTrigger />
+            {children}
+          </main>
+        </div>
+      </SidebarProvider>
+    </>
+  );
+}
